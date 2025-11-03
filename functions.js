e4e68740263ecb0575c5de39c7b3b28b9ede5aec
Marco Erin Fisher <marcofisher21@gmail.com>
2025-11-03 12:51:45 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+    let num1 = 2;
+    let num2 = 43;
+    let greeting = greet("Mark");
+    let sum = add(num1, num2);
+    console.log(greeting);
+    console.log("The sum is: " + sum);
